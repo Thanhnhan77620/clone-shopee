2f74e548fcf8ad0ff1200d430d7132e3621dfd9e
thanhnhan <thanhnhan77620@gmail.com>
2022-09-12 23:41:11 +0700

    Add component Brand into HomePage

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,38 +1,38 @@
 import classnames from 'classnames/bind';
 
 import { BrandHeader } from './BrandHeader';
-import { TopSearchItem } from '../TopSearch';
-import styleTopSearchItem from '../TopSearch/TopSearchItem/TopSearchItem.module.scss';
+import styleBrandItem from '../Brand/BrandItem/BrandItem.module.scss';
 import styles from './Brand.module.scss';
 import Slider from '../Slider';
 import Banner from '../Banner/Banner';
+import BrandItem from './BrandItem/BrandItem';
 
 const cx = classnames.bind(styles);
-const cxTopSearchItem = classnames.bind(styleTopSearchItem);
+const cxBrandItemItem = classnames.bind(styleBrandItem);
 function Brand({ listBanner = [] }) {
     const data = [];
     const item = {
-        name: 'Áo Thun Form Rộng Ngắn Tay Unisex Áo Thun Form Rộng Ngắn Tay Unisex',
-        imageURL: 'https://cf.shopee.vn/file/96ebd654d5f96234c688dffdc5702a71',
+        name: 'Giảm tới 10%',
+        imageURL: 'https://cf.shopee.vn/file/7de40171008ed7ab10b6236928b0a161_xhdpi',
     };
     const total = 15;
     for (let i = 1; i <= total; i++) {
         data.push({ ...item, id: i });
     }
     return (
-        <div className={cx('section-top-search')}>
+        <div className={cx('section-brand')}>
             <BrandHeader />
             <div className={cx('body')}>
                 <div className={cx('body-right')}>
-                    <Banner type="carousel" data={listBanner} />
+                    <Banner type="carousel" data={listBanner} height={551} showIndicators={true}/>
                 </div>
                 <div className={cx('body-left')}>
                     <Slider
                         listItem={data}
-                        step={5}
-                        defaultInfiniteItems={6}
-                        CarouselItemComponent={TopSearchItem}
-                        cssCarouselItem={cxTopSearchItem('carousel__item')}
+                        step={3}
+                        defaultInfiniteItems={4}
+                        CarouselItemComponent={BrandItem}
+                        cssCarouselItem={cxBrandItemItem('carousel__item')}
                     />
                 </div>
             </div>
--- src/components/SideBarFilter/SideBarFilter.jsx
+++ src/components/SideBarFilter/SideBarFilter.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import classnames from 'classnames/bind';
 
-function SideBarFilter(props) {
-    return <div>Side bar</div>;
+import styles from './SideBarFilter.module.scss';
+
+const cx = classnames.bind(styles);
+function SideBarFilter() {
+    return (
+        <>
+            <nav className={cx('category')}>
+                <h3 className={cx('category__heading')}>
+                    <i className={cx('category__heading-icon', 'fa-solid fa-list')}></i>
+                    Danh mục
+                </h3>
+
+                <ul className={cx('category-list')}>
+                    <li className={cx('category-item', 'category-item--active')}>
+                        <a href="/" className={cx('category-item__link')}>
+                            Trang điểm mặt
+                        </a>
+                    </li>
+                    <li className={cx('category-item')}>
+                        <a href="/" className={cx('category-item__link')}>
+                            Trang điểm môi
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
 }
 
 export default SideBarFilter;
