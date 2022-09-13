67d3d94c54678d7a1ac49391595c427e3ec382d9
thanhnhan <thanhnhan77620@gmail.com>
2022-09-14 00:10:31 +0700

    Add-done sidebar filter

--- src/components/SideBarFilter/SideBarFilter.jsx
+++ src/components/SideBarFilter/SideBarFilter.jsx
@@ -1,4 +1,8 @@
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classnames from 'classnames/bind';
+import Button from '../Button';
+import RatingStar from '../RatingStar';
 
 import styles from './SideBarFilter.module.scss';
 
@@ -11,7 +15,6 @@ function SideBarFilter() {
                     <i className={cx('category__heading-icon', 'fa-solid fa-list')}></i>
                     Danh mục
                 </h3>
-
                 <ul className={cx('category-list')}>
                     <li className={cx('category-item', 'category-item--active')}>
                         <a href="/" className={cx('category-item__link')}>
@@ -24,7 +27,62 @@ function SideBarFilter() {
                         </a>
                     </li>
                 </ul>
+
+                <div className={cx('brand-group')}>
+                    <span className={cx('brand-header')}>Thương Hiệu</span>
+                    <ul className={cx('brand-list')}>
+                        <li className={cx('brand-item')}>
+                            <div className={cx('brand-item__checkbox')}></div>
+                            <div className={cx('brand-item__link')}>Trang điểm mặt</div>
+                        </li>
+                        <li className={cx('brand-item', 'brand-item--active')}>
+                            <div className={cx('brand-item__checkbox')}>
+                                <FontAwesomeIcon icon={faCheck} />
+                            </div>
+                            <div className={cx('brand-item__link')}>Trang điểm mặt</div>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className={cx('brand-group')}>
+                    <span className={cx('brand-header')}>Khoảng Giá</span>
+                    <ul className={cx('brand-list')}>
+                        <li className={cx('brand-item')}>
+                            <input type="text" defaultValue={0} className={cx('brand-item__input')} />
+                            <span className={cx('brand-item__icon')}>-</span>
+                            <input type="text" defaultValue={10} className={cx('brand-item__input')} />
+                        </li>
+                    </ul>
+                </div>
+
+                <Button primary normal className={cx('custom-button')}>
+                    ÁP DỤNG
+                </Button>
+
+                <div className={cx('brand-group')}>
+                    <span className={cx('brand-header')}>Đánh Giá</span>
+                    <ul className={cx('brand-list')}>
+                        <li className={cx('brand-item')}>
+                            <RatingStar size={20} score={5} colorFill="yellow" />
+                        </li>
+                        <li className={cx('brand-item')}>
+                            <RatingStar size={20} score={4} colorFill="yellow" />
+                        </li>
+                        <li className={cx('brand-item')}>
+                            <RatingStar size={20} score={3} colorFill="yellow" />
+                        </li>
+                        <li className={cx('brand-item')}>
+                            <RatingStar size={20} score={2} colorFill="yellow" />
+                        </li>
+                        <li className={cx('brand-item')}>
+                            <RatingStar size={20} score={1} colorFill="yellow" />
+                        </li>
+                    </ul>
+                </div>
             </nav>
+            <Button primary normal className={cx('custom-button')}>
+                XÓA TẤT CẢ
+            </Button>
         </>
     );
 }
